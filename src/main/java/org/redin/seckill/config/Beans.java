b1cc4aa0ee6eb6c52c5730695af8347f7f2b5eee
redinw <redinwer@gmail.com>
2017-10-06 10:31:30 +0800

    redisDao注册到bean容器

--- src/main/java/org/redin/seckill/config/Beans.java
+++ src/main/java/org/redin/seckill/config/Beans.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * Description:
  */
 @Configuration
-public class RedisConfig {
+public class Beans {
 
     @Bean
     public RedisDao redisDao(@Value("${spring.redis.host}") String ip, @Value("${spring.redis.port}") Integer port){
